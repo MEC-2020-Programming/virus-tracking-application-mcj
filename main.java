bd9c7e0f134e814afaa642d937aef8caf55ac694
Kareem-Elmokattaf <60820260+Kareem-Elmokattaf@users.noreply.github.com>
2020-11-07 14:01:55 -0500

    Add files via upload

--- main.java
+++ main.java
@@ -1,71 +1,45 @@
 package virustracking;
-import java.io.*;  
-import java.util.*;
+import java.io.*;
 
 public class main {
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-//		 try {
-			 System.out.println(getValues());
-///		 }
-//			 int myint;
-//			 	FileReader fr=new FileReader("/Users/karimelmokattaf/Desktop/testing.txt"); 
-//			 	
-//			 	BufferedReader br = new BufferedReader(fr);  
-//			 	StringBuffer countries = new StringBuffer(); 
-//			 	
-//			 	int counter = 0;
-//			 	String line;  
-//			 	
-//			 	while((line=br.readLine())!=null)  
-//			 	{  
-//			 		if(counter == 0)
-//			 		{
-//			 			countries.append(line);     
-//			 			++counter;
-//				 		if(counter == 1)
-//				 		{   
-//				 			countries.append("\n");   
-//				 			counter = 0;
-//				 		}
-//			 		}
-//			 	}  
-//			 	fr.close();    
-//			 	System.out.println("Contents of File: ");  
-//			 	System.out.println(countries.toString());  
-//			 }  
-//			 catch(IOException e)  
-//			 {  
-//				 e.printStackTrace();  
-//			 } 
-			
-		 }
+	try {
+		 StringBuilder sb = new StringBuilder();
+		 FileReader fr=new FileReader("/Users/karimelmokattaf/Desktop/testing.txt"); 
+		 BufferedReader br = new BufferedReader(fr);  
+		 StringBuffer countries = new StringBuffer();   
+		 FileWriter csvout = new FileWriter("trial8.csv");
+		 String line = "Country,Total Cases, New Cases, Total Deaths, Total Recovered, Active Cases";  
+		 	countries.append(line);
+		 	countries.append("\n");
+		 while((line=br.readLine())!=null)  
+		 {  
+			 
+			line = line.replace(",", "");
+		 	line = line.replace("(", "");
+		 	line = line.replace(")", "");
+		 	line = line.replace("[", "");
+		 	line = line.replace("]", ",");
+		 	line = line.replace("{", "");
+		 	line = line.replace("}", "");
+		 	line = line.replace("\"", "");
+		 	
+		 	countries.append(line.substring(0,(line.length())-1));
+		 	countries.append("\n");
+		 }  
+		 csvout.append(countries);
+		 csvout.flush();
+		 csvout.close();
+		 fr.close();    
+		 System.out.println("Contents of File: ");  
+		 System.out.println(countries.toString());  
+		}  
+	catch(IOException e)  
+		{  
+			e.printStackTrace();  
+		} 
+		
+	 }
 	
-	public static ArrayList<String> getValues() {
-        FileInputStream stream = null;
-        try {
-            stream = new FileInputStream("/Users/karimelmokattaf/Desktop/testing.txt");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        String strLine;
-        ArrayList<String> lines = new ArrayList<String>();
-        try {
-            while ((strLine = reader.readLine()) != null) {
-                String lastWord = strLine.substring(strLine.lastIndexOf(" ")+1);
-                lines.add(lastWord);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return lines;
-    }
 }
-
-
